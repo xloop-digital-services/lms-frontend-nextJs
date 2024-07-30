dec284e551ca119b21b5b4678f7ba87c94ab4288
L-javeria <bintemurad2000@gmail.com>
2024-07-30 17:54:43 +0500

    implement dashboad component

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -8,6 +8,7 @@ function page() {
       {/* <div className="p-4">
         <div className="text-center lg:p-10 py-10 px-4 mt-20 max-md:mt-40 max-lg:mt-44"></div>
       </div> */}
+      
     </>
   );
 }
