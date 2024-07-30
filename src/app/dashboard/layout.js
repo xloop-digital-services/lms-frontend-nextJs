550114a93faa63d6273788ffc3b28da325ea1a03
sarahp27 <sarah.patel@xloopdigital.com>
2024-07-31 01:12:40 +0500

    sidebar fixed

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,16 +2,16 @@ import React from "react";
 import Navbar from "@/components/Navbar";
 import { SidebarProvider } from "@/providers/useSidebar";
 import SideBar from "@/components/SideBar";
-import Sidebar from "@/components/Sidebar";
+
 export default function DashboardLayout({ children }) {
   return (
     <>
       <SidebarProvider>
         {/* <div> */}
-          <Navbar />
+        <Navbar />
         {/* </div> */}
         <SideBar />
-        {/* <Sidebar /> */}
+
         {children}
       </SidebarProvider>
     </>
