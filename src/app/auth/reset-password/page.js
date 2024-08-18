78af57769726b96278a7ce4f6d734280ce5595f3
sarahp27 <sarah.patel@xloopdigital.com>
2024-08-18 13:26:26 +0500

    testing done

--- src/app/auth/reset-password/page.js
+++ src/app/auth/reset-password/page.js
@@ -1,120 +1,120 @@
-"use client";
-import axios from "axios";
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
-import Cookies from "js-cookie";
-import { resetPassword } from "@/api/route";
+// "use client";
+// import axios from "axios";
+// import React, { useState } from "react";
+// import { useRouter } from "next/navigation";
+// import Cookies from "js-cookie";
+// import { resetPassword } from "@/api/route";
 
-export default function Page() {
-  const [email, setEmail] = useState("");
-  // const API = process.env.NEXT_PUBLIC_BACKEND_URL;
-  // const Token = Cookies.get("access-token");
-  const router = useRouter();
+// export default function Page() {
+//   const [email, setEmail] = useState("");
+//   // const API = process.env.NEXT_PUBLIC_BACKEND_URL;
+//   // const Token = Cookies.get("access-token");
+//   const router = useRouter();
 
-  async function handleResetPassword() {
-    try {
-    const response = await resetPassword(email);
-      if (response.data.status_code === 200) {
-        toast.success(response.data.message, {
-          position: "top-right",
-          autoClose: 2000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          onClose: () => {
-            setEmail("");
-            router.push("/auth/login");
-          },
-        });
-      } else {
-        toast.error("Wrong Email.", response.data.message, {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-      }
-    } catch (error) {
-      console.error("Error during login:", error);
-      toast.error("An error occurred during login.", {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    }
-  }
+//   async function handleResetPassword() {
+//     try {
+//     const response = await resetPassword(email);
+//       if (response.data.status_code === 200) {
+//         toast.success(response.data.message, {
+//           position: "top-right",
+//           autoClose: 2000,
+//           hideProgressBar: false,
+//           closeOnClick: true,
+//           pauseOnHover: true,
+//           draggable: true,
+//           progress: undefined,
+//           onClose: () => {
+//             setEmail("");
+//             router.push("/auth/login");
+//           },
+//         });
+//       } else {
+//         toast.error("Wrong Email.", response.data.message, {
+//           position: "top-right",
+//           autoClose: 5000,
+//           hideProgressBar: false,
+//           closeOnClick: true,
+//           pauseOnHover: true,
+//           draggable: true,
+//           progress: undefined,
+//         });
+//       }
+//     } catch (error) {
+//       console.error("Error during login:", error);
+//       toast.error("An error occurred during login.", {
+//         position: "top-right",
+//         autoClose: 5000,
+//         hideProgressBar: false,
+//         closeOnClick: true,
+//         pauseOnHover: true,
+//         draggable: true,
+//         progress: undefined,
+//       });
+//     }
+//   }
 
-  return (
-    <div className="w-full  max-w-lg mx-auto p-6 mt-[10%] font-inter">
-      <div className="mt-7 bg-surface-100 rounded-xl shadow-lg dark:bg-dark-800 dark:border-dark-700 ">
-        <div className="p-4 sm:p-7">
-          <div className="text-center">
-            <h1 className="block text-2xl font-bold text-[#282323] dark:text-white font-exo">
-              Forgot password?
-            </h1>
-            <p className="mt-2 text-sm text-dark-600 dark:text-dark-400">
-              Remember your password?
-              <a
-                className="text-[#03A1D8] decoration-1 hover:underline font-medium pl-1 cursor-pointer"
-                href="/auth/login"
-              >
-                Login here
-              </a>
-            </p>
-          </div>
+//   return (
+//     <div className="w-full  max-w-lg mx-auto p-6 mt-[10%] font-inter">
+//       <div className="mt-7 bg-surface-100 rounded-xl shadow-lg dark:bg-dark-800 dark:border-dark-700 ">
+//         <div className="p-4 sm:p-7">
+//           <div className="text-center">
+//             <h1 className="block text-2xl font-bold text-[#282323] dark:text-white font-exo">
+//               Forgot password?
+//             </h1>
+//             <p className="mt-2 text-sm text-dark-600 dark:text-dark-400">
+//               Remember your password?
+//               <a
+//                 className="text-[#03A1D8] decoration-1 hover:underline font-medium pl-1 cursor-pointer"
+//                 href="/auth/login"
+//               >
+//                 Login here
+//               </a>
+//             </p>
+//           </div>
 
-          <div className="mt-5">
-            <form>
-              <div className="grid gap-y-5">
-                <div className="space-y-3">
-                  <label
-                    for="email"
-                    className="block text-sm font-medium leading-5 text-dark-700"
-                  >
-                    Email address
-                  </label>
-                  <div className="relative">
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      placeholder="email address"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      className="py-3 px-4 block w-full outline-none border border-dark-200 rounded-md text-sm focus:border-blue-300 focus:ring-[#03A1D8] shadow-sm"
-                      required
-                      aria-describedby="email-error"
-                    />
-                  </div>
-                  <p
-                    className="hidden text-xs text-[#ca0f0f] mt-2"
-                    id="email-error"
-                  >
-                    Please include a valid email address so we can get back to
-                    you
-                  </p>
-                </div>
-                <button
-                  type="submit"
-                  onClick={handleResetPassword}
-                  className="w-full flex justify-center py-3 px-4 text-sm font-medium rounded-lg text-dark-100 bg-[#03A1D8] hover:bg-[#2799bf] focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
-                >
-                  Reset password
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+//           <div className="mt-5">
+//             <form>
+//               <div className="grid gap-y-5">
+//                 <div className="space-y-3">
+//                   <label
+//                     for="email"
+//                     className="block text-sm font-medium leading-5 text-dark-700"
+//                   >
+//                     Email address
+//                   </label>
+//                   <div className="relative">
+//                     <input
+//                       type="email"
+//                       id="email"
+//                       name="email"
+//                       placeholder="email address"
+//                       value={email}
+//                       onChange={(e) => setEmail(e.target.value)}
+//                       className="py-3 px-4 block w-full outline-none border border-dark-200 rounded-md text-sm focus:border-blue-300 focus:ring-[#03A1D8] shadow-sm"
+//                       required
+//                       aria-describedby="email-error"
+//                     />
+//                   </div>
+//                   <p
+//                     className="hidden text-xs text-[#ca0f0f] mt-2"
+//                     id="email-error"
+//                   >
+//                     Please include a valid email address so we can get back to
+//                     you
+//                   </p>
+//                 </div>
+//                 <button
+//                   type="submit"
+//                   onClick={handleResetPassword}
+//                   className="w-full flex justify-center py-3 px-4 text-sm font-medium rounded-lg text-dark-100 bg-[#03A1D8] hover:bg-[#2799bf] focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+//                 >
+//                   Reset password
+//                 </button>
+//               </div>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
