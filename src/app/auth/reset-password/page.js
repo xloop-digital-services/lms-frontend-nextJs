22539c39f42de02e30ab42fd41d0046066741544
L-javeria <bintemurad2000@gmail.com>
2024-10-05 03:39:36 +0500

    theme modified

--- src/app/auth/reset-password/page.js
+++ src/app/auth/reset-password/page.js
@@ -71,7 +71,7 @@ export default function Page() {
             <p className="mt-2 text-sm text-dark-600">
               Remember your password?
               <a
-                className="text-[#03A1D8] decoration-1 hover:underline font-medium pl-1 cursor-pointer"
+                className="text-blue-300 decoration-1 hover:underline font-medium pl-1 cursor-pointer"
                 href="/auth/login"
               >
                 Login here
@@ -94,7 +94,7 @@ export default function Page() {
                       placeholder="email address"
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
-                      className="py-3 px-4 block w-full outline-none border border-dark-200 rounded-md text-sm focus:border-blue-300 focus:ring-[#03A1D8] shadow-sm"
+                      className="py-3 px-4 block w-full outline-none border border-dark-200 rounded-md text-sm focus:border-blue-300 focus:ring-blue-300 shadow-sm"
                       required
                     />
                   </div>
@@ -109,7 +109,7 @@ export default function Page() {
                 <button
                   type="submit"
                   onClick={handleResetPassword}
-                  className="w-full flex justify-center py-3 px-4 text-sm gap-4 font-medium rounded-lg text-dark-100 bg-[#03A1D8] hover:bg-[#2799bf] focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                  className="w-full flex justify-center py-3 px-4 text-sm gap-4 font-medium rounded-lg text-dark-100 bg-blue-300 hover:bg-[#3272b6] focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
                 >
                   {loading && (
                     <CircularProgress size={20} style={{ color: "white" }} />
